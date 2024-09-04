14aeaf187a9ee2d306cb4e6ed3342a83c4232068
shaobingme <codemold@gmail.com>
2024-09-04 22:50:32 +0800

    补齐丢失的composer文件

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'shaobingme/startmvc',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '430165126207e1a6c67a8124ad6ff055d16dd668',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'shaobingme/startmvc' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '430165126207e1a6c67a8124ad6ff055d16dd668',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
